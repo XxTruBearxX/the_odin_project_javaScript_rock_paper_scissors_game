ec528260c166851c980021b9db457004bbbfabd4
XxTruBearxX <98701856+XxTruBearxX@users.noreply.github.com>
2025-11-12 18:37:01 -0700

    created html and js files for rock paper scissors game
    filled out html boilerplate and filled js file with pseudocode

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # the_odin_project_javaScript_rock_paper_scissors_game
 Assignment from The Odin Project to write a Rock, Paper, Scissors game in JavaScript
+
+This is my first attempt at a JavaScript program and my first time using pseudocode
---
+++ rps.js
@@ -0,0 +1,27 @@
+//a simple game of rock, paper, scissors
+//establish player and computer scores
+//create array of choices
+//create function using random number generator to select computer choice
+//set random number generator to pull choices from array
+//create function to get player choice
+//create function to get computer choice
+//create function to play a round
+//create event listeners for player choices
+//start if/else statements to determine winner of round
+  //if player choice is rock
+    //if computer choice is rock - tie
+    //elseif computer choice is paper - computer wins
+    //else computer choice is scissors - player wins
+  //if player choice is paper
+    //if computer choice is rock - player wins
+    //elseif computer choice is paper - tie
+    //else computer choice is scissors - comuter wins
+  //if player choice is scissors
+    //if computer choice is rock - computer wins
+    //elseif computer choice is paper - player wins
+    //else computer choice is scissors - tie
+//end if/else statements
+//create function to update scores and display results
+//create function to reset game
+//declare winner when a score reaches 5
+//end game
